3a695be66ed323f4837a15e04169bb6e77bd068d
Igor Abreu Araujo <i-abreu@hotmail.com>
2017-08-01 01:21:06 -0300

    Novas Alterações para inserir Bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector : 'my-app',
-    template : '<h1>My First Angular2 App</h1>'
+    template : `<h1>My First Angular2 App</h1>
+    <button class='btn-sucess'>Hello Bootstrap</button>
+    `
 })
 
 export class AppComponent {}
