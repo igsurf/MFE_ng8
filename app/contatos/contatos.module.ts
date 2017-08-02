58221a6475f6bb40145055a3407e3cf61450749c
Igor Abreu Araujo <i-abreu@hotmail.com>
2017-08-02 00:56:46 -0300

    Exibição da lista usando Shadow DOM

---
+++ app/contatos/contatos.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {ContatosListaComponent} from './contatos-lista.component';
+
+@NgModule({
+    declarations: [ContatosListaComponent
+    
+    ]
+
+})
+
+export class ContatosModule{}
