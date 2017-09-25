faa4a958e7e310cac386391d3ef8aa822fa17645
Igor Abreu Araujo <i-abreu@hotmail.com>
2017-09-24 22:45:50 -0300

    Alterações importantes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: `
-    <h1>My First Angular2 App</h1>
-    <router-outlet></router-outlet>
-    `
+    templateUrl: 'app.component.html'
 })
 
 export class AppComponent { }
